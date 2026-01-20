38b523fe5d0979a931d2e93d3249d1bf9d83579f
Torge Schöwing <ts@netzmal.de>
2026-01-20 23:36:00 +0100

    Ex. 1.13-1.14

--- anecdotes/src/App.jsx
+++ anecdotes/src/App.jsx
@@ -18,13 +18,30 @@ const App = () => {
         'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
         'The only way to go fast, is to go well.'
     ]
-
+    const [votes, setVotes] = useState(anecdotes.map(() => 0));
     const [selected, setSelected] = useState(0)
+    const [maxIndex, setMaxIndex] = useState(0)
+
+    const handleVote = (index) => {
+        const copy = [...votes];
+        copy[index] += 1;
+        setVotes(copy);
+        if (copy[index] > copy[maxIndex]) {
+            setMaxIndex(index);
+        }
+    }
+
 
     return (
         <div>
+            <h2>Anecdote of the day</h2>
             <p>{anecdotes[selected]}</p>
+            <p>has {votes[selected]} votes</p>
+            <Button text={"vote"} onClick={() => handleVote(selected)}/>
             <Button text={"next anecdote"} onClick={()=>setSelected(Math.floor(Math.random() * (anecdotes.length)))} />
+            <h2>Anecdote of the day</h2>
+            <p>{anecdotes[maxIndex]}</p>
+            <p>has {votes[maxIndex]} votes</p>
         </div>
     )
 }
