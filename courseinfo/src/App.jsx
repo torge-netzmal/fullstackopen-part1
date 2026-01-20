975356258f6241bbac95f159548fe6796a118137
Torge Schöwing <ts@netzmal.de>
2026-01-20 21:05:33 +0100

    Ex. 1.1-1.2

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -8,15 +8,9 @@ const Header = (props) => {
 const Content = (props) => {
     return (
         <>
-            <p>
-                {props.parts[0]} {props.exercises[0]}
-            </p>
-            <p>
-                {props.parts[1]} {props.exercises[1]}
-            </p>
-            <p>
-                {props.parts[2]} {props.exercises[2]}
-            </p>
+            <Part part={props.parts[0]} exercise={props.exercises[0]}/>
+            <Part part={props.parts[1]} exercise={props.exercises[1]}/>
+            <Part part={props.parts[2]} exercise={props.exercises[2]}/>
         </>
     );
 }
@@ -28,6 +22,16 @@ const Total = (props) => {
             <p>Number of exercises {props.total}</p>
         </>
     );
+}
+
+const Part = (props) => {
+    return (
+        <>
+            <p>
+                {props.part} {props.exercise}
+            </p>
+        </>
+    )
 }
 
 const App = () => {
