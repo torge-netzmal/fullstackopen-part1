7fd56cd691d8ff038819801719386726c0300b5f
Torge Schöwing <ts@netzmal.de>
2026-01-20 23:03:51 +0100

    Ex. 1.11

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -18,7 +18,13 @@ const Feedback = (props) => {
 }
 
 const StatisticLine = (props) => {
-    return (<p>{props.text} {props.value}</p>)
+    return (
+            <tr>
+                <td>{props.text}</td>
+                <td>{props.value}</td>
+            </tr>
+
+    )
 }
 
 const Statistics = (props) => {
@@ -33,12 +39,17 @@ const Statistics = (props) => {
     return (
         <div>
             <h2>statistics</h2>
-            <StatisticLine text={"good"} value={props.good}/>
-            <StatisticLine text={"neutral"} value={props.neutral}/>
-            <StatisticLine text={"bad"} value={props.bad}/>
-            <StatisticLine text={"all"} value={all}/>
-            <StatisticLine text={"average"} value={average}/>
-            <StatisticLine text={"positive"} value={positive}/>
+            <table>
+                <tbody>
+                <StatisticLine text={"good"} value={props.good}/>
+                <StatisticLine text={"neutral"} value={props.neutral}/>
+                <StatisticLine text={"bad"} value={props.bad}/>
+                <StatisticLine text={"all"} value={all}/>
+                <StatisticLine text={"average"} value={average}/>
+                <StatisticLine text={"positive"} value={positive}/>
+                </tbody>
+            </table>
+
         </div>
     )
 }
